0e4435e05c517c28a74a29bc4f25b317a5f380f6
Maurice <mauricegma@gmail.com>
2024-10-22 14:26:50 -0300

    lets go

--- CG/main.cpp
+++ CG/main.cpp
@@ -35,8 +35,13 @@ int main(int argc, char *argv[]) {
     // Bordas (Poligonos)
     QVector<QPoint> borda_ext = { QPoint(110, 0), QPoint(0, 110), QPoint(220, 110) };
     displayFile.addObject(DisplayObject("Externa", PolygonType, borda_ext));
-    QVector<QPoint> borda_int = { QPoint(70, 60), QPoint(150, 60), QPoint(110, 100) };
-    displayFile.addObject(DisplayObject("Interna", PolygonType, borda_int));
+
+    QVector<QPoint> quadrado = { QPoint(210,0), QPoint(260,0), QPoint(260, 50), QPoint(210, 50) };
+    displayFile.addObject(DisplayObject("quadrado", PolygonType, quadrado));
+    QVector<QPoint> losango = { QPoint(300,300), QPoint(200,200), QPoint(300,100), QPoint(400,200) };
+    displayFile.addObject(DisplayObject("losango", PolygonType, losango));
+    QVector<QPoint> rupee = { QPoint(500,600), QPoint(600,500), QPoint(600,400), QPoint(500,300),QPoint(400,400),QPoint(400,500) };
+    displayFile.addObject(DisplayObject("rupee", PolygonType, rupee));
 
     // Criando o Tela
     Tela* canvas = new Tela();
@@ -44,7 +49,7 @@ int main(int argc, char *argv[]) {
 
     // centralizando o CanvasWidget
     window.setCentralWidget(canvas);
-    window.resize(220, 115);
+    window.resize(640,640);
     window.show();
 
     return app.exec();
