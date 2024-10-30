09aae481d11c7ef18ed06e1cb4bd8d13eb4d5db6
Vinicius Hashizume <viniciushashizume26@gmail.com>
2024-10-30 19:52:36 -0300

    update matriz
    transformei o codigo de vetor para matriz

--- CG/Matriz.h
+++ CG/Matriz.h
@@ -5,21 +5,17 @@
 #include <QVector>
 
 class Pontos {
-
 public:
     double x, y;
 
     // Construtor para inicializar os pontos
     Pontos(double x_val, double y_val) : x(x_val), y(y_val) {}
-
 };
 
 void centroGeometrico(QVector<Pontos>& objeto, double& xsave, double& ysave);
-
-void transladar(QVector<Pontos>& objeto, double X, double Y);
-
-void escalonar(QVector<Pontos>& objeto, double X, double Y);
-
-void rotacionar(QVector<Pontos>& objeto, double X);
+void transladar(QVector<Pontos>& objeto, double dx, double dy);
+void escalonar(QVector<Pontos>& objeto, double sx, double sy);
+void rotacionar(QVector<Pontos>& objeto, double angulo);
+void aplicarMatriz(QVector<Pontos>& objeto, const QVector<QVector<double>>& matriz);
 
 #endif // MATRIZ_H
